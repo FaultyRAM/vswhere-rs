d9c7d24977ec5ee2bb27f65b2c396b692d7665e0
FaultyRAM <316014+FaultyRAM@users.noreply.github.com>
2021-05-02 18:26:13 +1000

    feat!: `Config::requires`

--- src/lib.rs
+++ src/lib.rs
@@ -21,19 +21,21 @@
 
 #[derive(Clone, Debug)]
 /// Controls vswhere invocation via builder-style configuration.
-pub struct Config<'a, 'b> {
+pub struct Config<'a, 'b, 'c, 'd> {
     all: bool,
     prerelease: bool,
     products: Option<&'a [&'b str]>,
+    requires: Option<Requires<'c, 'd>>,
 }
 
-impl<'a, 'b> Config<'a, 'b> {
+impl<'a, 'b, 'c, 'd> Config<'a, 'b, 'c, 'd> {
     /// Creates a new invocation builder with default parameters.
     pub const fn new() -> Self {
         Self {
             all: false,
             prerelease: false,
             products: None,
+            requires: None,
         }
     }
 
@@ -63,10 +65,29 @@ impl<'a, 'b> Config<'a, 'b> {
         self.products = value;
         self
     }
+
+    /// If `Some`, vswhere will use the given component/workload ID allowlist to limit the returned
+    /// instances.
+    ///
+    /// The default value for this setting is `None`.
+    pub fn requires(&mut self, value: Option<Requires<'c, 'd>>) -> &mut Self {
+        self.requires = value;
+        self
+    }
 }
 
-impl<'a, 'b> Default for Config<'a, 'b> {
+impl<'a, 'b, 'c, 'd> Default for Config<'a, 'b, 'c, 'd> {
     fn default() -> Self {
         Self::new()
     }
+}
+
+#[derive(Clone, Debug)]
+/// A component/workload ID allowlist.
+pub enum Requires<'a, 'b> {
+    /// An allowlist that includes only instances with at least one of the given component or
+    /// workload IDs.
+    Any(&'a [&'b str]),
+    /// An allowlist that includes only instances with all of the given component or workload IDs.
+    All(&'a [&'b str]),
 }
